e80fa08614a920bc4609fce2a8bd26204e092987
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-12-20 09:59:51 +0530

    first commit

---
+++ BlogSite/src/test/java/lk/tdm/BlogSite/BlogSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.tdm.BlogSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
