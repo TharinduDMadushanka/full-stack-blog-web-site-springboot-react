01c90f411b4dbb234c5986ddc5d407ad362a6e2a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2025-03-19 20:11:21 +0530

    function to loop pargraph over 2 seconds

--- blog-frontend/src/pages/Home/Home.jsx
+++ blog-frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import './Home.css'
-
-import video2 from '../../assets/home/video2.mp4'
-import arrow_btn from '../../assets/home/arrow_btn.png'
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+import video2 from '../../assets/home/video2.mp4';
+import arrow_btn from '../../assets/home/arrow_btn.png';
 import arrow_icon from '../../assets/home/arrow_icon.svg';
-
 import cat_1 from '../../assets/home/life_style.jpg';
 import cat_2 from '../../assets/home/movie.jpg';
 import cat_3 from '../../assets/home/science.jpg';
@@ -12,14 +10,9 @@ import cat_4 from '../../assets/home/sports.jpg';
 import cat_5 from '../../assets/home/tech.jpg';
 import cat_6 from '../../assets/home/travel.jpg';
 import Footer from '../../components/Footer/Footer';
-import Navbar from '../../components/Navbar/Navbar';
 import { useNavigate } from 'react-router-dom';
 
-
 const Home = () => {
-
-  // State to keep track of the hovered category
-
   const navigate = useNavigate();
 
   // Array of paragraphs
@@ -28,40 +21,28 @@ const Home = () => {
     "Science",
     "Travel",
     "Entertainment",
-    // "Discover the amazing features we offer."
   ];
 
   // State to keep track of the current paragraph index
   const [currentParagraph, setCurrentParagraph] = useState(0);
 
-  // Use useEffect to change the paragraph every 5 seconds
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrentParagraph((prevParagraph) => (prevParagraph + 1) % paragraphs.length);
-  //   }, 3000); // 5 seconds interval
+  // Use useEffect to change the paragraph every 2 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentParagraph((prevParagraph) => (prevParagraph + 1) % paragraphs.length);
+    }, 2000); // 2 seconds interval
 
-  //   return () => clearInterval(interval); // Cleanup interval on component unmount
-  // }, [paragraphs.length]);
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [paragraphs.length]);
 
   return (
-    <div className='home'>
-
-      {/* <Navbar/> */}
-
+    <div className='home container-fluid'>
       <div className='nav'>
-      <div className="nav-logo">TDM</div>
-
-      <ul className="nav-menu">
-        {/* Wrapping navigation call inside an anonymous function */}
-        {/* <li onClick={() => navigate('/')}>Home</li>
-        <li onClick={()=> navigate('/tech')}>Technology</li>
-        <li>Science</li>
-        <li onClick={() => navigate('/travel')}>Travel</li>
-        <li>Entertainment</li>
-        <li>About me</li> */}
-        <button className='nav-contact' onClick={() =>navigate('/user-login')}>Get Started</button>
-      </ul>
-    </div>
+        <div className="nav-logo">TDM</div>
+        <ul className="nav-menu">
+          <button className='nav-contact' onClick={() => navigate('/user-login')}>Get Started</button>
+        </ul>
+      </div>
 
       <video className='background-video' autoPlay loop muted>
         <source src={video2} type='video/mp4' />
@@ -83,46 +64,35 @@ const Home = () => {
         <h2 className="category-title">Categories</h2>
 
         <div className="category-grid">
-
           <div className="category-box">
             <img src={cat_1} alt="Lifestyle" />
           </div>
-
           <div className="category-box">
             <img src={cat_2} alt="Movies" />
           </div>
-
           <div className="category-box">
             <img src={cat_3} alt="Science" />
           </div>
-
           <div className="category-box">
             <img src={cat_4} alt="Sports" />
           </div>
-
           <div className="category-box">
             <img src={cat_5} alt="Tech" />
           </div>
-
           <div className="category-box">
             <img src={cat_6} alt="Travel" />
           </div>
-
         </div>
 
         <div className="cat-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt="" />
+          <p>Show More</p>
+          <img src={arrow_icon} alt="" />
         </div>
-
       </div>
 
-      <Footer/>
-
+      <Footer />
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Home
+export default Home;
